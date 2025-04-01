559e93fd6ba81beaf0727e189581eff5d4514201
kimdh0363 <kimdh0363@naver.com>
2025-04-01 18:35:36 +0900

    Feat: SpringDataJpa를 사용한 리포지토리 구현

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -3,6 +3,7 @@ package hello.hello_spring;
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,20 +12,22 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager entityManager;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(entityManager);
-    }
+//        return new JpaMemberRepository(entityManager);
+//
+//    }
 }
--- src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -20,7 +20,6 @@ class MemberServiceIntegrationTest {
 
 
     @Test
-    @Commit
     void 회원가입() {
         //given
         Member member = new Member();
