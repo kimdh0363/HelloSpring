7ed88179acb625e992bd778585962f8ff182c397
kimdh0363 <kimdh0363@naver.com>
2025-04-01 13:10:17 +0900

    순수 JDBC 구현

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -35,4 +35,10 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
